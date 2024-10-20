0035a0e34c93299d3dd0dbd1c1f5a64497ef3df7
MicroBlock <66859419+std-microblock@users.noreply.github.com>
2024-10-20 11:47:51 +0800

    feat: implement allocator

--- include/blook/Process.h
+++ include/blook/Process.h
@@ -4,68 +4,87 @@
 #include "dirty_windows.h"
 #include "utils.h"
 #include <cstdint>
+#include <expected>
 #include <filesystem>
 #include <iostream>
 #include <optional>
 #include <span>
-#include <expected>
 #include <string>
 #include <vector>
 
 namespace blook {
-    class Pointer;
+class Pointer;
 
-    class Process : public std::enable_shared_from_this<Process> {
+class Process : public std::enable_shared_from_this<Process> {
 #ifdef _WIN32
 
-        DWORD pid;
+  DWORD pid;
 
-        explicit Process(HANDLE h);
+  explicit Process(HANDLE h);
 
-        explicit Process(DWORD pid);
+  explicit Process(DWORD pid);
 
 #endif
 
-        std::optional<Pointer> _memo{};
+  struct Allocator {
+    explicit Allocator(std::shared_ptr<Process> proc);
 
-        explicit Process(std::string name);
+    void *allocate(size_t size, void *nearAddr = nullptr);
 
-    protected:
-        HANDLE h = nullptr;
-        friend Module;
-        friend Pointer;
+    void deallocate(void *addr);
 
-    public:
-        CLASS_MOVE_ONLY(Process)
-
-        [[nodiscard]] std::optional<std::vector<std::uint8_t>>
-        read(void *addr, size_t size) const;
-
-        void *read(void *dest, void *addr, size_t size) const;
-
-        std::expected<void, std::string> write(void *addr, std::span<uint8_t>) const;
-
-        [[nodiscard]] std::optional<std::shared_ptr<Module>>
-        module(const std::string &name);
-
-        std::map<std::string, std::shared_ptr<Module>> modules();
-
-        // Return the current (dll) module
-        [[nodiscard]] std::optional<std::shared_ptr<Module>> module();
-
-        // Return the current process module
-        [[nodiscard]] std::optional<std::shared_ptr<Module>> process_module();
-
-        [[nodiscard]] bool is_self() const;
-
-        static std::shared_ptr<Process> self();
-
-        Pointer memo();
-
-        template<class... T>
-        static std::shared_ptr<Process> Process::attach(T &&...argv) {
-            return std::shared_ptr<Process>(new Process(argv...));
-        }
+  private:
+    struct AllocatedPageData {
+      size_t size;
+      std::map<void *, size_t> allocated;
     };
+    std::map<void *, AllocatedPageData> allocatedPages;
+    std::shared_ptr<Process> proc;
+  };
+
+  std::optional<Pointer> _memo{};
+  std::optional<Allocator> _allocator{};
+
+  explicit Process(std::string name);
+
+protected:
+  HANDLE h = nullptr;
+  friend Module;
+  friend Pointer;
+
+public:
+  CLASS_MOVE_ONLY(Process)
+
+  [[nodiscard]] std::optional<std::vector<std::uint8_t>>
+  read(void *addr, size_t size) const;
+
+  void *read(void *dest, void *addr, size_t size) const;
+
+  std::expected<void, std::string> write(void *addr, std::span<uint8_t>) const;
+
+  [[nodiscard]] std::optional<std::shared_ptr<Module>>
+  module(const std::string &name);
+
+  std::map<std::string, std::shared_ptr<Module>> modules();
+
+  // Return the current (dll) module
+  [[nodiscard]] std::optional<std::shared_ptr<Module>> module();
+
+  // Return the current process module
+  [[nodiscard]] std::optional<std::shared_ptr<Module>> process_module();
+
+  [[nodiscard]] bool is_self() const;
+
+  static std::shared_ptr<Process> self();
+
+  Pointer memo();
+
+  Allocator allocator();
+
+  template <class... T>
+  static std::shared_ptr<Process> Process::attach(T &&...argv) {
+    return std::shared_ptr<Process>(new Process(argv...));
+  }
+};
 
 } // namespace blook
