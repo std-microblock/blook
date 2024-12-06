2335cb6ed9492a97bc1eb40eca39709b8557521f
MicroBlock <66859419+std-microblock@users.noreply.github.com>
2024-12-06 13:39:12 +0800

    feat(xref): support x86

--- include/blook/disassembly.h
+++ include/blook/disassembly.h
@@ -25,27 +25,7 @@ namespace blook {
 
             auto operator->() const { return &instr; }
 
-            [[nodiscard]] std::vector<Pointer> xrefs() const {
-                using namespace zasm;
-                std::vector<Pointer> ptrs;
-                for (std::size_t i = 0; i < instr.getOperandCount(); i++) {
-                    const auto &op = instr.getOperand(i);
-
-                    if (const Mem *opMem = op.getIf<Mem>(); opMem != nullptr) {
-                        if (opMem->getBase() == x86::rip) {
-                            auto ptr = (void *) opMem->getDisplacement();
-                            ptrs.emplace_back(ptr);
-                        }
-                    }
-
-                    if (const Imm *opImm = op.getIf<Imm>(); opImm != nullptr) {
-                        const auto data = opImm->value<size_t>();
-                        ptrs.emplace_back((void *) data);
-                    }
-                }
-
-                return ptrs;
-            }
+            [[nodiscard]] std::vector<Pointer> xrefs() const;
         };
 
         template<typename Range>
--- src/disassembly.cpp
+++ src/disassembly.cpp
@@ -3,6 +3,29 @@
 //
 
 #include "blook/disassembly.h"
+#include <iostream>
 
-namespace blook {
-} // blook
+namespace blook::disasm {
+std::vector<Pointer> InstructionCtx::xrefs() const {
+  using namespace zasm;
+  std::vector<Pointer> ptrs{};
+  for (std::size_t i = 0; i < instr.getOperandCount(); i++) {
+    const auto &op = instr.getOperand(i);
+
+    if (const Mem *opMem = op.getIf<Mem>(); opMem != nullptr) {
+      if (opMem->getBase() == x86::rip || opMem->getBase() == x86::eip) {
+        auto ptr = (void *)opMem->getDisplacement();
+        std::cout << "RIP/EIP: " << ptr << std::endl;
+        ptrs.emplace_back(ptr);
+      }
+    }
+
+    if (const Imm *opImm = op.getIf<Imm>(); opImm != nullptr) {
+      const auto data = opImm->value<size_t>();
+      ptrs.emplace_back((void *)data);
+    }
+  }
+
+  return ptrs;
+}
+} // namespace blook::disasm
